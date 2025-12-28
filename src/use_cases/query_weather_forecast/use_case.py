6d4ec83ebd0e860fad738977ef8010469f07a419
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 13:31:20 +0100

    GREEN: handle success case when weather forecast exists

--- src/use_cases/query_weather_forecast/use_case.py
+++ src/use_cases/query_weather_forecast/use_case.py
@@ -1,18 +1,8 @@
 from src.use_cases.query_weather_forecast.output_dto import OutputDTO
-from src.use_cases.query_weather_forecast.boundaries.weather_forecast_repository_interface import (
-    WeatherForecastRepositoryInterface,
-)
-from src.use_cases.query_weather_forecast.boundaries.presenter_interface import (
-    PresenterInterface,
-)
 
 
 class QueryWeatherForecastUseCase:
-    def __init__(
-        self,
-        weather_forecast_repository: WeatherForecastRepositoryInterface,
-        presenter: PresenterInterface,
-    ):
+    def __init__(self, weather_forecast_repository, presenter):
         self.weather_forecast_repository = weather_forecast_repository
         self.presenter = presenter
 
@@ -20,8 +10,10 @@ class QueryWeatherForecastUseCase:
         forecast = self.weather_forecast_repository.get(input_dto.city)
 
         if forecast is None:
-            output = OutputDTO(
-                success=False,
-                weather_data=None,
+            self.presenter.present(
+                OutputDTO(success=False, weather_data=None)
             )
-            self.presenter.present(output)
+        else:
+            self.presenter.present(
+                OutputDTO(success=True, weather_data=forecast)
+            )
