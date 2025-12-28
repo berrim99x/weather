493f8f4e497ee7bfe5f78801953466d8ef38fe81
berrim99x <hakimhakim20046@gmail.com>
2025-12-28 13:32:30 +0100

    BLUE: refactoring

--- src/use_cases/query_weather_forecast/use_case.py
+++ src/use_cases/query_weather_forecast/use_case.py
@@ -1,19 +1,35 @@
 from src.use_cases.query_weather_forecast.output_dto import OutputDTO
+from src.use_cases.query_weather_forecast.boundaries.weather_forecast_repository_interface import (
+    WeatherForecastRepositoryInterface,
+)
+from src.use_cases.query_weather_forecast.boundaries.presenter_interface import (
+    PresenterInterface,
+)
 
 
 class QueryWeatherForecastUseCase:
-    def __init__(self, weather_forecast_repository, presenter):
+    def __init__(
+        self,
+        weather_forecast_repository: WeatherForecastRepositoryInterface,
+        presenter: PresenterInterface,
+    ):
         self.weather_forecast_repository = weather_forecast_repository
         self.presenter = presenter
 
     def execute(self, input_dto):
         forecast = self.weather_forecast_repository.get(input_dto.city)
 
+        output = self._build_output(forecast)
+        self.presenter.present(output)
+
+    def _build_output(self, forecast):
         if forecast is None:
-            self.presenter.present(
-                OutputDTO(success=False, weather_data=None)
+            return OutputDTO(
+                success=False,
+                weather_data=None,
             )
-        else:
-            self.presenter.present(
-                OutputDTO(success=True, weather_data=forecast)
-            )
+
+        return OutputDTO(
+            success=True,
+            weather_data=forecast,
+        )
